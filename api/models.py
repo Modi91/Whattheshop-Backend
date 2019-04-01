a3c0ae004117ece864424656a605c0969c4811dc
Maram Kamil <maram.kamil@gmail.com>
2019-04-01 12:50:19 +0300

    initial models

--- api/models.py
+++ api/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+
+class Category (models.Model):
+    """docstring for category """
+    category = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.category
+
+
+class Product(models.Model):
+    """docstring for Prodect"""
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    description = models.TextField()
+    stock = models.IntegerField()
+    img = models.ImageField(upload_to='img_Prodect', null = True)
+    category =  models.ManyToManyField( Category, related_name='prodects')
