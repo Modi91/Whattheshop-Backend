aea6f09e081bc84af931ddbc042ad4f846dd59c7
Maram Kamil <maram.kamil@gmail.com>
2019-04-11 13:48:24 +0300

    fvjkfdjhv

--- api/models.py
+++ api/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     description = models.TextField()
     stock = models.IntegerField()
